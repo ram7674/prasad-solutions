1239e253e6d5090aedf48130d1405c1a3ba110e3
ram <ramakrishnachintha78@gmail.com>
2024-07-03 15:23:32 +0530

    add designs

--- js/main.js
+++ js/main.js
@@ -83,3 +83,15 @@
 
 })(jQuery);
 
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+        faqItem.addEventListener('click', () => {
+            faqItem.parentNode.classList.toggle('faq-active');
+        });
+    });
+});
+
